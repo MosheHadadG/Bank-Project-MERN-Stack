34f8d55a63401490ba150f196978e06bd0b91fcf
Moshe <102148360+MosheHadadG@users.noreply.github.com>
2022-06-26 13:55:39 +0300

    Update mongoose.js

--- server/app/db/mongoose.js
+++ server/app/db/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://mosheHadad:adNbSYU4bK74ZOv9@cluster0.os2qc.mongodb.net/bankAPI?retryWrites=true&w=majority", (err, mongoConnection) => {
+mongoose.connect(process.env.MongoURI, (err, mongoConnection) => {
   if(err) throw new Error('Mongoose Connection!!, Error: ' + err);
   if(!process.env.NODE_ENV) {
     const { port, host, name } = mongoConnection;
